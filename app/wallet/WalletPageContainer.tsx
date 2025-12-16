791ef0c7b9dbb63499a42b60bbf85bb461007274
Nuki4119 <jctrade11@gmail.com>
2025-12-16 16:59:13 -0500

    fix: added stub WalletOverview component to resolve build error

--- app/wallet/WalletPageContainer.tsx
+++ app/wallet/WalletPageContainer.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import UniversalPageLayout from '../../components/Layout/UniversalPageLayout';
-import WalletOverview from '../../components/blocks/WalletOverview';
+// import WalletOverview from '../../components/blocks/WalletOverview';
 import { Banknote, Upload, Download } from 'lucide-react';
 
 interface Transaction {
