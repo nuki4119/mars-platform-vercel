eef37eee850db3b753c82e40c5ba3ca33073f998
Nuki4119 <jctrade11@gmail.com>
2025-12-16 12:57:52 -0500

    fix: corrected broken import for UniversalPageLayout

--- app/create/post/PostFormContainer.tsx
+++ app/create/post/PostFormContainer.tsx
@@ -1,7 +1,7 @@
 // app/create/post/PostFormContainer.tsx
 
 import React from 'react';
-import UniversalPageLayout from '../../feed/UniversalPageLayout';
+import UniversalPageLayout from '../../../components/Layout/UniversalPageLayout';
 import PostForm from '@/components/forms/PostForm';
 
 export default function PostFormContainer() {
