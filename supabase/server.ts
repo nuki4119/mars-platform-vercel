8d563945224d4f03576e79bc80ad573b9ce999b8
Nuki4119 <jctrade11@gmail.com>
2025-12-16 17:54:42 -0500

    fix: switch to createServerComponentClient now that latest helper is installed

--- supabase/server.ts
+++ supabase/server.ts
@@ -1,11 +1,10 @@
 // supabase/server.ts
 
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
-import { type Database } from './types/supabase'; // Optional: if you have typed DB schema
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 
 export function createClient() {
-  return createServerComponentClient<Database>({
+  return createServerComponentClient({
     cookies,
   });
 }
